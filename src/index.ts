0464afbf6eac5a30d2a121a4e88d2313b166996a
SriramSololearner <sriram.gsr16@gmail.com>
2024-08-28 13:25:21 +0530

    client deployed url updates in socket config

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,7 @@ const server = http.createServer(app);
 
 export const io = new Server(server, {
   cors: {
-    origin:"http://localhost:3000",
+    origin: "https://chat-app-react-kappa-six.vercel.app",
     methods: ["GET", "POST"],
     credentials: true,
   },
