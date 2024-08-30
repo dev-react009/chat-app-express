4f2cf5a20c32352cf1703f3aba6765ca9ea83507
SriramSololearner <sriram.gsr16@gmail.com>
2024-08-30 22:50:04 +0530

    new changes in email template

--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -7,15 +7,15 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-});
+}); 
 
 export const sendWelcomeEmail = async (to: string, name: string) => {
   const mailOptions = {
     from: process.env.EMAIL_USER,
     to,
-    subject: `Hello ${name},\n\nWelcome to our chat app! We are glad to have you on board.\n\nBest regards,\nChat App Team`,
-    html: "Welcome to Our Chat App!",
-    text: `<p>Hello ${name},</p> <br> <p>We're thrilled to welcome you to our Chat App community! 🎉</p> <br> <p>Thank you for joining us. We're excited to have you on board and can't wait for you to start exploring all the features we've designed to help you connect, chat, and have fun.</p> <br>  <p>If you ever need assistance, don't hesitate to reach out. We're here to help you make the most out of your experience.</p> <br>  <b>Happy chatting!</b><p>Best regards,<br> <b> Chat App Team</b></p>`,
+    html: `Hello ${name},\n\nWelcome to our chatCloudHub! We're thrilled to welcome you to our ChatCloud community! 🎉\n\nBest regards,\nChat App Team \n\n Thank you for joining us. We're excited to have you on board and can't wait for you to start exploring all the features we've designed to help you connect, chat, and have fun.\n\n If you ever need assistance, don't hesitate to reach out. We're here to help you make the most out of your experience.\n\nHappy chatting! \n\n Best regards, \n\n ChatCloudHubTeam`,
+    subject: "Welcome to Our ChatCloudHub!",
+    text: `<p>Hello ${name},</p> <br> <p>We're thrilled to welcome you to our ChatCloud community! 🎉</p> <br> <p>Thank you for joining us. We're excited to have you on board and can't wait for you to start exploring all the features we've designed to help you connect, chat, and have fun.</p> <br>  <p>If you ever need assistance, don't hesitate to reach out. We're here to help you make the most out of your experience.</p> <br>  <b>Happy chatting!</b><p>Best regards,<br> <b> ChatCloudHubTeam</b></p>`,
   };
 
   try {
